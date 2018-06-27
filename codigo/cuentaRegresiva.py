98dc06259427d9c4a5549273e47b8832aa8be124
Maxi Chaves <maximiliano.e.chaves@gmail.com>
2018-06-26 23:46:47 -0300

    Update

---
+++ codigo/cuentaRegresiva.py
@@ -0,0 +1,9 @@
+from time import sleep
+contador = 10
+
+while contador > 0:
+	print (contador)
+	contador = contador -1
+	sleep(1)
+
+print ("Despegueeeeeeeasjkdhakjsdhaskjdas")
--- codigo/nota.py
+++ codigo/nota.py
@@ -1,9 +1,10 @@
 nota = 0
 
 print ("Que te sacaste?")
-num = input()
+nota = input()
 
 if nota > 6:
 	print ("Aprobaste")
 else:
 	print ("Desaprobaste")
+
